23123d1706d606934d8bb93e906263728fe7dab2
Carmelo <sammarcocarmelo@gmail.com>
2021-06-25 23:37:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md') as readme_file:
     long_description = readme_file.read()
 
 setup(name='GPSconverter',
-      version='0.0.4',
+      version='0.0.5',
       description='Python package for manipulating gps data and easily convert them to other different formats.',
       long_description=long_description,
       long_description_content_type='text/markdown',
